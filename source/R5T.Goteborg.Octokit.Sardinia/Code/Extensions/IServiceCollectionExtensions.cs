9aa28efb98355d13b3eadd92b66db954fc05a84d
MinexAutomation <minex.automation@gmail.com>
2019-12-14 03:00:01 -0800

    Added IServiceCollection extension to use GitHub configuration.

---
+++ source/R5T.Goteborg.Octokit.Sardinia/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Sardinia;
+
+using R5T.Goteborg.Octokit.Sardinia.Configuration;
+
+using RawGitHubConfiguration = R5T.Goteborg.Octokit.Sardinia.Configuration.Raw.GitHubConfiguration;
+
+
+namespace R5T.Goteborg.Octokit.Sardinia
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static IServiceCollection UseGitHubConfiguration(this IServiceCollection services)
+        {
+            services
+                .AddOptions()
+                .Configure<RawGitHubConfiguration>()
+                .ConfigureOptions<GitHubConfigurationConfigureOptions>()
+                ;
+
+            return services;
+        }
+    }
+}
